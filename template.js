60ebbe6d495bc34c3f121be7c5783cb52d2d9177
utkukantekin <utkukantekin97@gmail.com>
2022-06-24 23:03:46 +0300

    modals revize

--- template.js
+++ template.js
@@ -19,3 +19,27 @@ $(function(){
 // login madal
 ////////////////////////////////
 
+$(function(){
+
+    let fillMail = function(){
+        $("#userCol").toggleClass("deneme");
+        $("#userCol").toggle();
+        $("#mailCol").toggleClass("col-md-4 col-md-8");
+    }
+
+    let fillUser = function(){
+        $("#mailCol").toggleClass("deneme");
+        $("#mailCol").toggle();
+        $("#userCol").toggleClass("col-md-4 col-md-8");
+    }
+
+    $("#mail").click(fillMail);
+    $("#userName").click(fillUser);
+
+    $("#userLabel").click(()=>{$("#userCol").hasClass("deneme") ? fillMail():0});
+    $("#mailLabel").click(()=>{$("#mailCol").hasClass("deneme") ? fillUser():0});
+})
+
+////////////////////////////////
+// registry madal
+////////////////////////////////
